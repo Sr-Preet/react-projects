5afcb0698ddc12999cacd9876d2e1c575e657b7e
Sr-Preet <55445693+Sr-Preet@users.noreply.github.com>
2021-03-11 11:48:14 +0530

    Update SingleMovie.js

--- moviedb/src/pages/SingleMovie.js
+++ moviedb/src/pages/SingleMovie.js
@@ -1,45 +1,54 @@
 import React from 'react'
 import { Link, useParams } from 'react-router-dom'
+import Loading from '../components/Loading'
 
 function SingleMovie() {
   const { imdbID } = useParams('imdbID')
   const [movie, setMovie] = React.useState({})
+  const [loading, setLoading] = React.useState(true)
   const getMovie = async () => {
     const data = await (
       await fetch('https://omdbapi.com/?apikey=895952f1&i=' + imdbID)
     ).json()
     setMovie(data)
+    setLoading(false)
   }
   React.useEffect(() => {
     getMovie()
   }, [imdbID])
   return (
-    <section className='single-movie'>
-      <img src={movie.Poster} alt={movie.Title} />
-      <div className='single-movie-info'>
-        <h2>{movie.Title}</h2>
-        <p>
-          <h4 style={{ display: 'inline' }}>Plot: </h4>
-          {movie.Plot}
-        </p>
-        <h4>IMDB: {movie.imdbRating}</h4>
-        <h4>Released: {movie.Released}</h4>
-        <h4>Runtime: {movie.Runtime}</h4>
-        <h4>Genre : {movie.Genre}</h4>
-        <h4>Directed By: {movie.Director}</h4>
-        <h4>Cast: {movie.Actors}</h4>
-        <h4>Language: {movie.Language}</h4>
-        {movie.Type === 'movie' ? (
-          <h4>Box Office: {movie.BoxOffice}</h4>
-        ) : (
-          <h4>Type: {movie.Type}</h4>
-        )}
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <section className='single-movie'>
+          <img src={movie.Poster} alt={movie.Title} />
+          <div className='single-movie-info'>
+            <h2>{movie.Title}</h2>
+            <p>
+              <h4 style={{ display: 'inline' }}>Plot: </h4>
+              {movie.Plot}
+            </p>
+            <h4>IMDB: {movie.imdbRating}</h4>
+            <h4>Released: {movie.Released}</h4>
+            <h4>Runtime: {movie.Runtime}</h4>
+            <h4>Genre : {movie.Genre}</h4>
+            <h4>Directed By: {movie.Director}</h4>
+            <h4>Cast: {movie.Actors}</h4>
+            <h4>Language: {movie.Language}</h4>
+            {movie.Type === 'movie' ? (
+              <h4>Box Office: {movie.BoxOffice}</h4>
+            ) : (
+              <h4>Type: {movie.Type}</h4>
+            )}
 
-        <Link to='/' className='btn'>
-          back to movies
-        </Link>
-      </div>
-    </section>
+            <Link to='/' className='btn'>
+              back to movies
+            </Link>
+          </div>
+        </section>
+      )}
+    </>
   )
 }
 
